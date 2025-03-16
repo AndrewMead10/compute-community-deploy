dca9db8cabb1848096c19bcabf201ec0d69122ff
Andrew Mead <andrewmead@verizon.net>
2025-03-16 17:08:28 -0500

    add testing env

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import openai
+
+client = openai.OpenAI(api_key="ZzQA-LEl3-s4gV-R8Mj", base_url="http://localhost:8080/v1")
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello, world!"}]
+)
+
+print(response.choices[0].message.content)
